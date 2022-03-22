03d59e85712e25e024fccba38ff4720213157632
notsoyeezy <wojdalski.marcin2137@gmail.com>
2022-03-22 11:12:07 +0100

    Adding postgres settings

--- ZielonoNam/settings.py
+++ ZielonoNam/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ZielonoNam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
     }
 }
 
