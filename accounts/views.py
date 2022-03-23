bf51058293163a7fcd10a9e65266bb146b9c19bc
notsoyeezy <wojdalski.marcin2137@gmail.com>
2022-03-23 23:58:49 +0100

    Fixing register error

--- accounts/views.py
+++ accounts/views.py
@@ -52,10 +52,10 @@ def user_register_view(request):
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            file = request.FILES['profile_pic']
-            storage.child(PROFILE_PICS_DIR + file.name).put("media/profile_pics/" + file.name)
             user_obj = User.objects.get(username=user_form.instance.username)
-            user_obj.pp_cdn_url = storage.child(PROFILE_PICS_DIR + file).get_url(None)
+            file = user_obj.profile_pic
+            storage.child(PROFILE_PICS_DIR + file.name).put("media/" + file.name)
+            user_obj.pp_cdn_url = storage.child(PROFILE_PICS_DIR + file.name).get_url(None)
             user_obj.save()
             return HttpResponseRedirect(reverse('accounts:login'))
         else:
